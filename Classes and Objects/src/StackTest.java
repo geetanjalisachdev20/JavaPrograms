a7a4660835d75267ce4b5c24df3ecf9448a67ccc
Geetanjali <geetanjalisachdev1999@gmail.com>
2019-01-16 23:32:44 +0530

    Incomplete code

---
+++ Classes and Objects/src/StackTest.java
@@ -0,0 +1,24 @@
+//
+import java.util.Scanner;
+class Stack{
+	int a[]=new int[10];
+	void push(){
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the elements of stack:");
+		for(int i=0;i<10;i++)
+			a[i]=sc.nextInt();
+
+	}
+	void pop(){
+
+	}
+	void isEmpty(){
+
+	}
+	void isFull(){
+
+	}
+	void SpaceLeft(){
+		
+	}
+}
