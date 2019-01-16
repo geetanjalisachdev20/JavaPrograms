3840ab4792cc91cd35f7902eb388e79378145beb
Geetanjali <geetanjalisachdev1999@gmail.com>
2019-01-16 23:22:37 +0530

    incomplete code

---
+++ Classes and Objects/src/EmployeeTest2.java
@@ -0,0 +1,32 @@
+//Employee data in tabular form
+import java.util.Scanner;
+class Employee{
+	String nm,add;
+    int yoj;
+    double sal;
+    Employee(){
+    	nm="Geetanjali";
+    	add="Gurgaon";
+    	sal=100000;
+    	yoj=1/02/2018
+    }
+    Employee(String nm,String add,double sal,int yoj){
+    	this.nm=nm;
+    	this.add=add;
+    	this.sal=sal;
+    	this.yoj=yoj;
+    }
+    void displayEmployee(){
+
+    }
+}
+class EmployeeTest2{
+	Employee e=new Employee();
+	Scanner sc=new Scanner(System.in);
+	System.out.print("Enter the no. of employees: ");
+	int n=sc.nextInt();
+	String em[]=new S
+	System.out.print("Enter: ");
+
+
+}
